0c2374811f274c827526389083ab12649c80032d
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2024-05-04 18:03:58 -0600

    initial tower placement

--- TDdemo/Assets/Scripts/TowerPlacement.cs
+++ TDdemo/Assets/Scripts/TowerPlacement.cs
@@ -10,7 +10,6 @@ public class TowerPlacement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -18,12 +17,22 @@ public class TowerPlacement : MonoBehaviour
     {
         if (currentTower != null)
         {
+            Vector3Int cell = GridHandler.grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            currentTower.transform.position = GridHandler.grid.GetCellCenterWorld(cell);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentTower.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+                currentTower = null;
+            }
         }
+        
     }
 
     public void SetTowerToPlace(GameObject tower)
     {
         currentTower = Instantiate(tower, Vector2.zero, Quaternion.identity);
+        currentTower.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
     }
 
 }
